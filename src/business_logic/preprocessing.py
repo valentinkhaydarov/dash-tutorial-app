24c7dbb609619b616aa8ca1b6e8ce50a2e7e2359
Valentin Khaydarov <valentin.khaydarov@capgemini.com>
2024-07-29 16:57:20 +0200

    update

--- src/business_logic/preprocessing.py
+++ src/business_logic/preprocessing.py
@@ -3,12 +3,23 @@ import pandas as pd
 from src.data_layer.fetch_data import get_population_data
 
 def get_country_list() -> pd.DataFrame:
-    # Provides a list of countries available in the dataset
+    """Provides a list of countries available in the dataset
+
+    Returns:
+        pd.DataFrame: List of unique countries from the dataset
+    """
     data = get_population_data()
     return data['country'].unique()
 
 def prepare_plot_data(country: str) -> pd.DataFrame:
-    # Provides data with population changes for the plot
+    """Provides data with population changes for the plot
+
+    Args:
+        country (str): Country name.
+
+    Returns:
+        pd.DataFrame: Plot data for the given country.
+    """
     data = get_population_data()
     country_data = data[data['country']==country]
     plot_data = country_data.copy()
--- src/data_layer/fetch_data.py
+++ src/data_layer/fetch_data.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
 def get_population_data()->pd.DataFrame:
-    # Provides the raw dataset
+    """Provides the raw dataset
+
+    Returns:
+        pd.DataFrame: Population data from different countries
+    """
     return pd.read_csv('data/gapminder_unfiltered.csv')
